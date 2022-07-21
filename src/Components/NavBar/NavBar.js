e3bc14dd6f12d2e4989d3cfed763320b64f54a7d
unknown <adek236@gmail.com>
2022-07-21 16:03:04 +0200

    Navbar styled for larger screens.

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import MenuIcon from "@mui/icons-material/Menu";
-import DiamondIcon from '@mui/icons-material/Diamond';
-import PersonIcon from '@mui/icons-material/Person';
+import DiamondIcon from "@mui/icons-material/Diamond";
+import PersonIcon from "@mui/icons-material/Person";
 import "./NavBar.css";
 // import { Link } from "react-router-dom";
 
@@ -9,38 +9,45 @@ function NavBar() {
   return (
     <div className="nav-bar">
       <nav className="nav-mobile">
-      <div>
-          <PersonIcon />
+        <div>
+          <a href="/">
+            <PersonIcon />
+          </a>
         </div>
         <div>
-          <DiamondIcon />
+          <a href="/">
+            <DiamondIcon />
+          </a>
         </div>
         <div>
           <MenuIcon />
         </div>
       </nav>
 
-      {/* 
       <nav className="nav-desktop">
-      <ul>
-        <li>
-          <a href="/"> HOME </a>
-        </li>
-        <li>
-          <a href="/"> NEWS </a>
-        </li>
-        <li>
-          <a href="/"> GAME </a>
-        </li>
-        <li>
-          <a href="/"> HEROES </a>
-        </li>
-        <li>
-          <a href="/"> SUPPORT </a>
-        </li>
-      </ul> 
+        <div className="flex-center">
+          <a href="/" className="flex-center">
+            <DiamondIcon />
+          </a>
+        </div>
+        <ul>
+          <li>
+            <a href="/" className="text-shadow"> HOME </a>
+          </li>
+          <li>
+            <a href="/" className="text-shadow"> NEWS </a>
+          </li>
+          <li>
+            <a href="/" className="text-shadow"> GUIDES </a>
+          </li>
+          <li>
+            <a href="/" className="text-shadow"> OFFERS </a>
+          </li>
+          <li>
+            <a href="/" className="text-shadow flex-center"> LOGIN <PersonIcon /> </a>
+          </li>
+        </ul>
       </nav>
-      */}
     </div>
   );
 }
